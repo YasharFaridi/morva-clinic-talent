2542d7f04248571a85192368acff36c5e6388598
YasharFRD <yasharfaridy@gmail.com>
2025-09-20 11:14:33 +0330

    add custom not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">صفحه پیدا نشد 😢</h1>
+      <p className="mt-2 text-gray-400">برگرد به صفحه اصلی</p>
+    </div>
+  );
+}
