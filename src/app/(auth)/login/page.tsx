61523be36b1cc3c975ab38568f1dc96b0791f707
YasharFRD <yasharfaridy@gmail.com>
2025-09-20 11:20:08 +0330

    refactor(layout): split into (main) and (auth) groups, add main and login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <form className="w-96 p-6 rounded shadow">
+      <h1 className="text-xl font-bold mb-4">ورود</h1>
+     LoginPage
+    </form>
+  );
+}
