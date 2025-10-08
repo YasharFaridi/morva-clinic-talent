737b3b2c9da927abdb3c423b45e232b338aa78c8
YasharFRD <yasharfaridy@gmail.com>
2025-10-08 20:43:55 +0330

    fix scroll bug!

--- src/app/morva-kids/page.tsx
+++ src/app/morva-kids/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import React, { useEffect } from "react";
 import MorvaKidsBanner from "@/components/morva-kids/MorvaKidsBanner";
-import React from "react";
 
 export default function MorvaKidsPage() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, []);
+
   return (
     <>
       <MorvaKidsBanner />
